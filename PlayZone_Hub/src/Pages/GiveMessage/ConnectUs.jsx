155d589627ceedde6191c840b37ba5998b47ffc6
Shakil53 <shakilahmed.dcc@gmail.com>
2024-09-17 12:33:38 +0600

    message through and Home and Banner page created

---
+++ PlayZone_Hub/src/Pages/GiveMessage/ConnectUs.jsx
@@ -0,0 +1,35 @@
+import Swal from 'sweetalert2';
+
+const ConnectUs = () => {
+
+  const requsetOnProcess = () => {
+    Swal.fire({
+      position: 'top-center',
+      icon: 'success',
+      title: 'Request Processing',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  }
+  return (
+    <div className=''>
+
+      <div className=''>
+
+
+        <div className='md:w-full text-center' >
+          <h2 className=' font-bold mb-2 text-black'>LET S TALK</h2>
+          <div className='md:w-2/4 mx-auto'>
+            <h2 className='text-4xl md:text-5xl font-bold text-slate-600 mb-10' >Let s make something great together. We are trusted by over 5000+ clients.</h2>
+          </div>
+          <p className='md:w-3/4 mb-5 font-semibold text-slate-600 mx-auto '>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+          <button onClick={requsetOnProcess} className='btn btn-wide'>Join Us</button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default ConnectUs;
---
+++ PlayZone_Hub/src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,28 @@
+import useTitle from "../../../hooks/useTitle";
+import Reviews from "../../CustomesRe/Reviews";
+import Gallery from "../../Gallery/Gallery";
+import ConnectUs from "../../GiveMessage/ConnectUs";
+import PhGallery from "../../PhGallery/PhGallery";
+import Tabls from "../../Tabs/Tabls";
+import Banner from "../Banner/Banner";
+
+
+const Home = () => {
+
+  useTitle("Toys Center|Home")
+  return (
+    <div>
+
+      <Banner></Banner>
+      <Gallery></Gallery>
+      <PhGallery></PhGallery>
+      <Tabls></Tabls>
+      <Reviews></Reviews>
+      <ConnectUs></ConnectUs>
+
+
+    </div>
+  );
+};
+
+export default Home;
