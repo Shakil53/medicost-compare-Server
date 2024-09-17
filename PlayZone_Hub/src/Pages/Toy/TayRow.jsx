af56a72adc2d43d90b1be6a5888942587a1f421f
Shakil53 <shakilahmed.dcc@gmail.com>
2024-09-17 12:52:44 +0600

    row of toy created

---
+++ PlayZone_Hub/src/Pages/Toy/TayRow.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+
+const ToyRow = ({ toy }) => {
+  const { _id, customerName, toyName, category, photo, price, quantity } = toy;
+  return (
+
+
+    <tr>
+      <th>
+
+      </th>
+      <td>
+        <div className="flex items-start">
+          <div className="avatar">
+
+          </div>
+          <div>
+            <div className="font-semibold">{customerName}</div>
+
+          </div>
+        </div>
+      </td>
+      <td>
+
+        {toyName}
+      </td>
+      <td>
+
+        {category}
+      </td>
+      <td>{"$ " + price}</td>
+      <td>{quantity}</td>
+      <th>
+
+        {/* <button className="btn btn-ghost btn-xs">details</button> */}
+
+        <Link to={`/toydetails/${_id}`}><button className="btn btn-ghost btn-xs">details</button></Link>
+      </th>
+     
+    </tr>
+
+
+  )
+}
+
+export default ToyRow;
