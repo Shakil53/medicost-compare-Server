966e377388e10788d50c2b06f5cc38620c8609a2
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-08 10:16:16 +0600

    toggle redio button and corresponding ui changes

--- todo-assisment/src/components/todo/TodoContainer.tsx
+++ todo-assisment/src/components/todo/TodoContainer.tsx
@@ -1,9 +1,12 @@
 import TodoCard from "./TodoCard";
 import AddTodoModal from "./AddTodoModal";
 import TodoFilter from "./TodoFilter";
+import { useAppSelector } from "@/redux/hook";
+
 
 
 const TodoContainer = () => {
+    const {todos} = useAppSelector((state) => state.todos)
     return (
         <div>
             <div className="flex justify-between">
@@ -12,12 +15,9 @@ const TodoContainer = () => {
             </div>
             <div className="bg-primary-gradient w-full rounded-xl p-[5px]">
                 <div className="bg-white p-4 w-full h-full space-y-3 rounded-lg">
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
-                    <TodoCard></TodoCard>
+                    {
+                        todos.map(item => <TodoCard {...item}></TodoCard>)
+                   }
                 </div>
             {/* <div className="bg-white flex justify-center items-center p-5">
                 <p className="text-2xl font-semibold">There is no task pending</p>
